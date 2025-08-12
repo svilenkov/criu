e6e90701cbe2d532631d7094d8dc4a8de44558aa
Igor Svilenkov Bozic <svilenkov@users.noreply.github.com>
2025-08-12 20:13:28 +0200

    criu/restore: gcs: adds restore implementation for Guarded Control Stack
    This commit finalizes AArch64 Guarded Control Stack (GCS)
    support by wiring the full dump and restore flow.
    
    The restore path adds the following steps:
    
     - Define shared AArch64 GCS types and constants in a dedicated header
       for both compel and CRIU inclusion
     - compel: add get/set NT_ARM_GCS via ptrace, enabling user-space
       GCS state save and restore.
     - During restore switch to the new GCS (via GCSSTR) to place capability
       token sa_restorer address
     - arch_shstk_trampoline() — We enable GCS in a trampoline that using
       prctl(PR_SET_SHADOW_STACK_STATUS, ...) via inline SVC. The trampoline
       ineeded because we can’t RET without a valid GCS.
     - restorer: map the recorded GCS VMA, populate contents top-down with
       GCSSTR, write the signal capability at GCSPR_EL0 and the valid token at
       GCSPR_EL0-8, then switch to the rebuilt GCS (GCSSS1)
     - Save and restore registers via ptrace
     - Extend restorer argument structures to carry GCS state
       into post-restore execution
     - Add shstk_set_restorer_stack(): sets tmp_gcs to temporary restorer
       shadow stack start
     - Add gcs_vma_restore implementation (required for mremap of the GCS VMA)
    
    Tested with:
        GCS_ENABLE=1 ./zdtm.py run -t zdtm/static/env00
    
    Signed-off-by: Igor Svilenkov Bozic <svilenkov@gmail.com>

--- compel/arch/aarch64/src/lib/infect.c
+++ compel/arch/aarch64/src/lib/infect.c
@@ -136,6 +136,26 @@ int compel_set_task_ext_regs(pid_t pid, user_fpregs_struct_t *ext_regs)
 	return 0;
 }
 
+int compel_set_task_gcs_regs(pid_t pid, user_fpregs_struct_t *ext_regs)
+{
+	struct iovec iov;
+
+	pr_info("gcs: restoring GCS registers for %d\n", pid);
+	pr_info("gcs: restoring GCS: gcspr=%llx features=%llx\n",
+			ext_regs->gcs.gcspr_el0, ext_regs->gcs.features_enabled);
+
+	iov.iov_base = &ext_regs->gcs;
+	iov.iov_len  = sizeof(ext_regs->gcs);
+
+	if (ptrace(PTRACE_SETREGSET, pid, NT_ARM_GCS, &iov)) {
+		pr_perror("gcs: Failed to set GCS registers for %d", pid);
+		return -1;
+	}
+
+	return 0;
+}
+
+
 int compel_syscall(struct parasite_ctl *ctl, int nr, long *ret, unsigned long arg1, unsigned long arg2,
 		   unsigned long arg3, unsigned long arg4, unsigned long arg5, unsigned long arg6)
 {
