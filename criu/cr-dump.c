cfa6c39b7e007732079cbab6175b35bf1afd3b12
Igor Svilenkov Bozic <svilenkov@users.noreply.github.com>
2025-08-12 18:56:41 +0200

    criu/dump: gcs: save GCS state during dump
    Add debug and info messages to log Guarded Control Stack state when
    dumping AArch64 threads. This includes the following values:
      - gcspr_el0
      - features_enabled
    
    Signed-off-by: Igor Svilenkov Bozic <svilenkov@gmail.com>

--- criu/cr-dump.c
+++ criu/cr-dump.c
@@ -846,6 +846,14 @@ static int dump_task_core_all(struct parasite_ctl *ctl, struct pstree_item *item
 	if (ret)
 		goto err;
 
+	if (core->ti_aarch64 && core->ti_aarch64->gcs) {
+		pr_debug("gcs: gcspr_el0: 0x%lx\n features_enabled: 0x%lx\n",
+			core->ti_aarch64->gcs->gcspr_el0,
+			core->ti_aarch64->gcs->features_enabled);
+	} else {
+		pr_info("gcs: core->ti_aarch64->gcs is not set\n");
+	}
+
 	img = img_from_set(cr_imgset, CR_FD_CORE);
 	ret = pb_write_one(img, core, PB_CORE);
 
@@ -1002,6 +1010,14 @@ static int dump_task_thread(struct parasite_ctl *parasite_ctl, const struct pstr
 	img = open_image(CR_FD_CORE, O_DUMP, tid->ns[0].virt);
 	if (!img)
 		goto err;
+
+	if (core->ti_aarch64 && core->ti_aarch64->gcs) {
+		pr_debug("gcs: gcspr_el0: 0x%lx\n features_enabled: 0x%lx\n",
+			core->ti_aarch64->gcs->gcspr_el0,
+			core->ti_aarch64->gcs->features_enabled);
+	} else {
+		pr_info("gcs: core->ti_aarch64->gcs is not set\n");
+	}
 
 	ret = pb_write_one(img, core, PB_CORE);
 
